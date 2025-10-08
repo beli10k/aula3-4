6c53d7bfdc46b040660b92ce05d96d2325eb2bbf
beli10k <isabeleferreira@colegiopaulodetarso.com.br>
2025-10-08 16:07:27 +0000

    adgsdf

---
+++ conjunos.py
@@ -0,0 +1,20 @@
+# 1. Lista de compras
+compras = ["pão", "manteiga", "queijo", "suco", "frutas"]
+
+# 2. Coordenadas Geográficas (x, y)
+coordenadas_rio = (-22.906847, -43.172896)
+
+# 3. CPFs dos clientes
+cpfs = {"555.666.777-88", "999.888.777-66", "123.321.123-12"}
+
+# 4. Notas dos alunos
+notas = [9.0, 6.5, 8.8, 7.2]
+
+# 5. Tarefas
+tarefas = ["Correr", "Estudar Python", "Assistir aula", "Cozinhar"]
+
+# 6. Meses do ano
+meses = ("Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez")
+
+# 7. Primeiro nome dos alunos
+alunos = {"Lucas", "Mariana", "Pedro", "Sofia"}
